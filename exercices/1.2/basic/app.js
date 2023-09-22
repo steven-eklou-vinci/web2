613124bdaa1d92d5abcf8bc63891c73885d095b8
Steven Eklou <steven.eklou@student.vinci.be>
2023-09-22 14:18:18 +0200

    modif

--- exercices/1.2/basic/app.js
+++ exercices/1.2/basic/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var filmsRouter = require ('./routes/films')
 
 var app = express();
 
@@ -17,6 +16,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/films', filmsRouter);
 
 module.exports = app;
