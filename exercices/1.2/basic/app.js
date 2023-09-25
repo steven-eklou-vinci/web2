6f5ffd63370ae8a7d275f4d1567478d3fc9b4725
steven.eklou <steven.eklou@student.vinci.be>
2023-09-25 22:09:20 +0200

    add new file in exercie 1.3

--- exercices/1.2/basic/app.js
+++ exercices/1.2/basic/app.js
@@ -3,9 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var filmsRouter = require ('./routes/films');
+var filmsRouter = require('./routes/films');
 
 var app = express();
 
@@ -13,51 +11,24 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-
-let getCounter=0;
+const stats = {};
 
 app.use((req, res, next) => {
-    console.log(req.methodvar express = require('express');
-    var path = require('path');
-    var cookieParser = require('cookie-parser');
-    var logger = require('morgan');
-    
-    var filmsRouter = require('./routes/films');
-    
-    var app = express();
-    
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    
-    const stats = {};
-    
-    app.use((req, res, next) => {
-      const currentOperation = `${req.method} ${req.path}`;
-      const currentOperationCounter = stats[currentOperation];
-      if (currentOperationCounter === undefined) stats[currentOperation] = 0;
-      stats[currentOperation] += 1;
-      const statsMessage = `Request counter : \n${Object.keys(stats)
-        .map((operation) => `- ${operation} : ${stats[operation]}`)
-        .join('\n')}
-          `;
-      console.log(statsMessage);
-      next();
-    });
-    
-    app.use(express.static(path.join(__dirname, 'public')));
-    
-    app.use('/films', filmsRouter);
-    
-    module.exports = app;);
-    
-    next();
-  });
+  const currentOperation = `${req.method} ${req.path}`;
+  const currentOperationCounter = stats[currentOperation];
+  if (currentOperationCounter === undefined) stats[currentOperation] = 0;
+  stats[currentOperation] += 1;
+  const statsMessage = `Request counter : \n${Object.keys(stats)
+    .map((operation) => `- ${operation} : ${stats[operation]}`)
+    .join('\n')}
+      `;
+  console.log(statsMessage);
+  next();
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/films',filmsRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/films', filmsRouter);
+
 module.exports = app;
--- exercices/1.3/basic/app.js
+++ exercices/1.3/basic/app.js
@@ -3,9 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var pizzaRouter = require('./routes/pizza');
+var filmsRouter = require('./routes/films');
 
 var app = express();
 
@@ -13,10 +11,24 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const stats = {};
+
+app.use((req, res, next) => {
+  const currentOperation = `${req.method} ${req.path}`;
+  const currentOperationCounter = stats[currentOperation];
+  if (currentOperationCounter === undefined) stats[currentOperation] = 0;
+  stats[currentOperation] += 1;
+  const statsMessage = `Request counter : \n${Object.keys(stats)
+    .map((operation) => `- ${operation} : ${stats[operation]}`)
+    .join('\n')}
+      `;
+  console.log(statsMessage);
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/pizza',pizzaRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
